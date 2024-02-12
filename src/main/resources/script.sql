0958eecd5ad184eb98bb0499a04f00e0f5233e88
macgarcia <marquim.iron@gmail.com>
2024-02-12 13:27:55 -0300

    CorreçÃões: script de insert, parâmetros de infra para os containers

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,5 +1,29 @@
-insert into cliente(id, limite, saldo_inicial) values(1, 100000, 0);
-insert into cliente(id, limite, saldo_inicial) values(2, 80000, 0);
-insert into cliente(id, limite, saldo_inicial) values(3, 1000000, 0);
-insert into cliente(id, limite, saldo_inicial) values(4, 10000000, 0);
-insert into cliente(id, limite, saldo_inicial) values(5, 500000, 0);
+INSERT INTO cliente(id, limite, saldo_inicial)
+SELECT 1, 100000, 0
+WHERE NOT EXISTS (
+    SELECT 1 FROM cliente WHERE id = 1
+);
+
+INSERT INTO cliente(id, limite, saldo_inicial)
+SELECT 2, 80000, 0
+WHERE NOT EXISTS (
+    SELECT 1 FROM cliente WHERE id = 2
+);
+
+INSERT INTO cliente(id, limite, saldo_inicial)
+SELECT 3, 1000000, 0
+WHERE NOT EXISTS (
+    SELECT 1 FROM cliente WHERE id = 3
+);
+
+INSERT INTO cliente(id, limite, saldo_inicial)
+SELECT 4, 10000000, 0
+WHERE NOT EXISTS (
+    SELECT 1 FROM cliente WHERE id = 4
+);
+
+INSERT INTO cliente(id, limite, saldo_inicial)
+SELECT 5, 500000, 0
+WHERE NOT EXISTS (
+    SELECT 1 FROM cliente WHERE id = 5
+);
